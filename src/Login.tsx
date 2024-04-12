a946b7c7299d8e0e2faaafaf8c59275df97ffc95
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2024-04-12 11:09:55 +0200

    create Login Component

---
+++ src/Login.tsx
@@ -0,0 +1,16 @@
+function Login() {
+  return (
+    <div>
+      <h2>Welcome to OpenChat</h2>
+      <div>
+        <input placeholder="username" />
+      </div>
+      <div>
+        <input placeholder="password" type="password" />
+      </div>
+      <button type="button">Login</button>
+    </div>
+  );
+}
+
+export default Login;
