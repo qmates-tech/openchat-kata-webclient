5ff344351000017ee20a6e50d224e48d2ba7a40e
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2024-05-10 17:12:55 +0200

    fix visual-studio-code React import errors

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom';
 import { render, screen } from "@testing-library/react";
-import App from "../src/App"
+import React from 'react';
+import App from "../src/App";
 
 test("renders the app component", () => {
   render(<App />);
