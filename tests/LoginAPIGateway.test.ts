dca7ce876eabc3cb22dfcf0f5774f5624128e29e
Daniele Megna <daniele.megna@qmates.tech>
2024-05-17 17:03:36 +0200

    refactor login api gateway tests
    extracted also an interceptor function

--- tests/LoginAPIGateway.test.ts
+++ tests/LoginAPIGateway.test.ts
@@ -1,6 +1,6 @@
 import { afterEach, describe, expect, it } from 'vitest'
 import { createLoginAPIGateway } from '../src/LoginAPIGateway'
-import { http, HttpResponse } from 'msw'
+import { http, HttpResponse, JsonBodyType, StrictResponse } from 'msw'
 import { setupServer } from 'msw/node'
 
 describe('Login API Gateway', () => {
@@ -11,13 +11,48 @@ describe('Login API Gateway', () => {
     mockServer.resetHandlers()
   })
 
+  it('returns user on right credentials', async () => {
+    interceptPost('/login', HttpResponse.json({
+      "id": "599dd5eb-fdea-4472-8baf-81ef7c18a2f1",
+      "username": "alessio89",
+      "about": "About Alessio user."
+    }))
+
+    const gateway = createLoginAPIGateway()
+
+    const loggedUser = await gateway.login('alessio89', 'correctPa$$word')
+
+    expect(loggedUser).toStrictEqual({
+      id: '599dd5eb-fdea-4472-8baf-81ef7c18a2f1',
+      username: 'alessio89',
+      about: 'About Alessio user.'
+    })
+  })
+
+  it('send properly request data to the API', async () => {
+    const interceptor = interceptPost('/login', HttpResponse.json({
+      "id": "0c85dc2f-6c7d-430d-a827-0e8aa30437d3",
+      "username": "dani90",
+      "about": "About Dani user."
+    }))
+
+    const gateway = createLoginAPIGateway()
+
+    await gateway.login('alessio89', 'thePassword')
+
+    expect(interceptor.receivedJsonBody()).toStrictEqual({
+      username: 'alessio89',
+      password: 'thePassword'
+    })
+  })
+
   it('throws invalid credentials error on wrong credentials', async () => {
-    mockServer.use(http.post('http://msw.mockapi.local/login', () =>
+    interceptPost('/login',
       new HttpResponse('Invalid credentials.', {
         status: 404,
         headers: { 'Content-Type': 'text/plain' },
       })
-    ))
+    )
 
     const gateway = createLoginAPIGateway()
 
@@ -26,42 +61,17 @@ describe('Login API Gateway', () => {
     }).rejects.toThrow(new Error("INVALID_CREDENTIALS"))
   })
 
-  it('returns user on right credentials', async () => {
-    mockServer.use(http.post('http://msw.mockapi.local/login', () =>
-      HttpResponse.json({
-        "id": "599dd5eb-fdea-4472-8baf-81ef7c18a2f1",
-        "username": "alessio89",
-        "about": "About Alessio user."
-      })
-    ))
-
-    const gateway = createLoginAPIGateway()
-
-    const loggedUser = await gateway.login('alessio89', 'correctPa$$word')
-
-    expect(loggedUser.id).toBe('599dd5eb-fdea-4472-8baf-81ef7c18a2f1')
-    expect(loggedUser.username).toBe('alessio89')
-    expect(loggedUser.about).toBe('About Alessio user.')
-  })
-
-  it('send properly request data to the API', async () => {
-    let sentJsonBody: any;
-    mockServer.use(http.post('http://msw.mockapi.local/login', async (request) => {
-      sentJsonBody = await request.request.json()
-      return HttpResponse.json({
-        "id": "599dd5eb-fdea-4472-8baf-81ef7c18a2f1",
-        "username": "alessio89",
-        "about": "About Alessio user."
-      })
+  function interceptPost(path: string, response: HttpResponse) {
+    let receivedJsonBody: any;
+    mockServer.use(http.post('http://msw.mockapi.local' + path, async (request) => {
+      receivedJsonBody = await request.request.json()
+      return response
     }))
 
-    const gateway = createLoginAPIGateway()
-
-    await gateway.login('alessio89', 'thePassword')
-
-    expect(sentJsonBody).toStrictEqual({
-      username: 'alessio89',
-      password: 'thePassword'
-    })
-  })
+    return {
+      receivedJsonBody() {
+        return receivedJsonBody
+      }
+    }
+  }
 })
