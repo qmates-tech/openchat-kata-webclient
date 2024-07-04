92befa7eef64f200f0788f1f7b5f39bfe731df50
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2024-07-04 22:44:05 +0200

    add form and autocomplete to be browser compliant

--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -1,11 +1,12 @@
-import { KeyboardEvent, useEffect, useRef, useState } from "react";
-import { Logout } from "../Logout/Logout";
-import './Login.css';
-import { LoginError, useLoginState } from "./LoginState";
+import { KeyboardEvent, useEffect, useRef, useState } from "react"
+import { Logout } from "../Logout/Logout"
+import './Login.css'
+import { LoginError, useLoginState } from "./LoginState"
 
 export function Login() {
   const { login, logout, isLoggingIn, loggedUser, loginError } = useLoginState()
   const [errorToShow, setErrorToShow] = useState<LoginError | undefined>(undefined)
+  const formRef = useRef<HTMLFormElement>(null)
   const usernameRef = useRef<HTMLInputElement>(null)
   const passwordRef = useRef<HTMLInputElement>(null)
 
@@ -17,22 +18,23 @@ export function Login() {
   }
   return (
     <article className="login">
-      <header><h3>Welcome to OpenChat</h3></header>
-      <div>
-        <input ref={usernameRef} placeholder="username" disabled={isLoggingIn}
-          onKeyDown={focusPasswordOnEnter} onChange={hideError} />
-      </div>
-      <div>
-        <input ref={passwordRef} type="password" placeholder="password" disabled={isLoggingIn}
-          onKeyDown={performLoginOnEnter} onChange={hideError} />
-      </div>
-      <footer>
-        <button type="button" aria-busy={isLoggingIn} disabled={isLoggingIn}
-          onClick={performLogin}>Login</button>
-        {errorToShow && <div className="error">{errorToShow}</div>}
-      </footer>
+      <form ref={formRef} autoComplete="on" onSubmit={submitLogin}>
+        <header><h3>Welcome to OpenChat</h3></header>
+        <div>
+          <input ref={usernameRef} autoComplete="on" name="username" placeholder="username" disabled={isLoggingIn}
+            onKeyDown={focusPasswordOnEnter} onChange={hideError} />
+        </div>
+        <div>
+          <input ref={passwordRef} autoComplete="on" name="password" placeholder="password" type="password" disabled={isLoggingIn}
+            onKeyDown={performLoginOnEnter} onChange={hideError} />
+        </div>
+        <footer>
+          <button type="submit" aria-busy={isLoggingIn} disabled={isLoggingIn}>Login</button>
+          {errorToShow && <div className="error">{errorToShow}</div>}
+        </footer>
+      </form>
     </article>
-  );
+  )
 
   function watchLoggedUser() {
     !loggedUser && usernameRef.current?.focus()
@@ -49,10 +51,6 @@ export function Login() {
     setErrorToShow(undefined)
   }
 
-  function performLogin() {
-    login(usernameRef.current?.value, passwordRef.current?.value)
-  }
-
   function focusPasswordOnEnter(e: KeyboardEvent<HTMLInputElement>): void {
     if (e.key === 'Enter') {
       e.preventDefault()
@@ -65,5 +63,14 @@ export function Login() {
       e.preventDefault()
       performLogin()
     }
+  }
+
+  function submitLogin(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+    performLogin()
+  }
+
+  function performLogin() {
+    login(usernameRef.current?.value, passwordRef.current?.value)
   }
 }
