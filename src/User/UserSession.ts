b4d8f39e587bdb4c9f7e3caa2fda24238d9903cf
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2024-07-05 16:04:04 +0200

    handle user session globally in the whole application

--- src/User/UserSession.ts
+++ src/User/UserSession.ts
@@ -1,6 +1,11 @@
 import { useEffect, useState } from "react";
 import { User } from "./User";
 
+const globalUserState = {
+  currentUser: undefined as User | undefined,
+  retrieving: true
+}
+
 export type UserSession = {
   currentUser: User | undefined;
   retrieving: boolean;
@@ -8,25 +13,37 @@ export type UserSession = {
 };
 export function useUserSession(): UserSession {
   const sessionName = "openChatSession"
-  const [user, setUser] = useState<User | undefined>()
-  const [retrieving, setRetrieving] = useState<boolean>(true)
+  const [user, setUser] = useState<User | undefined>(globalUserState.currentUser)
+  const [retrieving, setRetrieving] = useState<boolean>(globalUserState.retrieving)
 
   useEffect(() => {
-    const session = localStorage.getItem(sessionName) as string
-    session && setUser(JSON.parse(session))
-    setRetrieving(false)
+    if (globalUserState.currentUser === undefined) {
+      const session = localStorage.getItem(sessionName)
+      session && setGlobalUserInfo(JSON.parse(session))
+      setGlobalUserStatus(false)
+    }
   }, [])
 
   return {
     currentUser: user,
     retrieving,
     setUserSession: (user: User | undefined) => {
-      setUser(user)
+      setGlobalUserInfo(user)
       if (user) {
         localStorage.setItem(sessionName, JSON.stringify(user))
       } else {
         localStorage.removeItem(sessionName)
       }
     }
+  }
+
+  function setGlobalUserInfo(user: User | undefined) {
+    setUser(user)
+    globalUserState.currentUser = user
+  }
+
+  function setGlobalUserStatus(retrieving: boolean) {
+    setRetrieving(retrieving)
+    globalUserState.retrieving = retrieving
   }
 }
