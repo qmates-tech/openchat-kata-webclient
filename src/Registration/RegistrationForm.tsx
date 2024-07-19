f0d734ae99f3c2ac2a6e1c5ee48aab488e26df6f
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2024-07-19 15:34:40 +0200

    RegistrationForm: enable the button when all the fields are filled

--- src/Registration/RegistrationForm.tsx
+++ src/Registration/RegistrationForm.tsx
@@ -1,18 +1,41 @@
+import { useEffect, useRef, useState } from "react";
+
 export function RegistrationForm() {
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const repeatPasswordRef = useRef<HTMLInputElement>(null);
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  useEffect(validateForm, []);
+
   return (
     <form>
       <div>
-        <input autoComplete="username" name="username" placeholder="username" />
+        <input ref={usernameRef} required autoComplete="username" name="username" placeholder="username"
+          onChange={validateForm}
+        />
       </div>
       <div>
-        <input name="password" placeholder="write a password" type="password" />
+        <input ref={passwordRef} required name="password" placeholder="password" type="password"
+          onChange={validateForm}
+        />
       </div>
       <div>
-        <input name="password" placeholder="repeat the password" type="password" />
+        <input ref={repeatPasswordRef} required name="password" placeholder="repeat password" type="password"
+          onChange={validateForm}
+        />
       </div>
       <footer>
-        <button type="submit" disabled={true}>Register</button>
+        <button type="submit" disabled={!isFormValid}>Register</button>
       </footer>
     </form>
   );
+
+  function validateForm() {
+    const isValid = !!usernameRef?.current?.value
+      && !!passwordRef?.current?.value
+      && !!repeatPasswordRef?.current?.value;
+
+    setIsFormValid(isValid);
+  }
 }
--- tests/Registration/RegistrationForm.test.tsx
+++ tests/Registration/RegistrationForm.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { RegistrationForm } from '../../src/Registration/RegistrationForm';
 
 describe('RegistrationForm Component', () => {
@@ -8,4 +9,26 @@ describe('RegistrationForm Component', () => {
 
     expect(screen.getByRole('button')).toHaveAttribute('disabled');
   })
+
+  it('enable the button when all the fields are filled', async () => {
+    render(<RegistrationForm />);
+
+    await userEvent.type(usernameInput(), "an user");
+    await userEvent.type(passwordInput(), "a password");
+    await userEvent.type(repeatPasswordInput(), "a password");
+
+    expect(screen.getByRole('button')).not.toHaveAttribute('disabled');
+  })
 })
+
+function usernameInput() {
+  return screen.getByPlaceholderText('username');
+}
+
+function passwordInput() {
+  return screen.getByPlaceholderText('password');
+}
+
+function repeatPasswordInput() {
+  return screen.getByPlaceholderText('repeat password');
+}
