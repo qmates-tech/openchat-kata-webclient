260e020789d9a3e387e4584d80ce3b4e051edacf
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2024-08-23 12:39:24 +0200

    remove fake login response

--- src/Login/LoginAPI.ts
+++ src/Login/LoginAPI.ts
@@ -10,9 +10,6 @@ export type LoginAPI = {
 export function createLoginAPI(baseUrl: string = Env.loginUrl): LoginAPI {
   return {
     async login(username: string, password: string): Promise<User> {
-      if (username === "correct" && password === "right") {
-        return Promise.resolve({ id: "1", username: "John Doe", about: "Hi there! I'm the administrator here.\nI've been in administration for over 15 years and I love what I do.\nMy goal is to create a positive, supportive environment where everyone can thrive. I believe in teamwork and open communication. Feel free to come to me with anything - I'm here to listen, help out, and support you.\nExcellence is a core value. I'm always looking to improve and deliver great results. At the same time, I know perfection isn't possible, and I'll be there for you through the ups and downs.\nOutside of work, you can find me playing Ice Hockey. I also enjoy giving back to the community through Extreme Programming User Groups.\nI'm grateful to be part of this team, and I can't wait to see what we accomplish together.\nLet me know if you ever need anything!" });
-      }
       const response = await postRequest(`${baseUrl}/login`, {
         username: username,
         password: password
