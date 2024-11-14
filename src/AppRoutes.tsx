cd3e762bc7848dada804a510a2f4723783b6b010
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2024-11-14 19:15:09 +0100

    add private timeline page

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -7,12 +7,13 @@ import { WallPage } from "./Wall/WallPage";
 import { OnlyPublicRoute } from "./App/OnlyPublicRoute.tsx";
 import { PostsListStateProvider } from "./Post/PostsList/PostsListState.tsx";
 
-export type RouteName = 'login' | 'registration' | 'wall';
+export type RouteName = 'login' | 'registration' | 'wall' | 'timeline';
 
 const routes: Record<RouteName, RouteProps> = {
   login: { path: '/login', element: <OnlyPublicRoute><LoginPage /></OnlyPublicRoute> },
   wall: { path: '/', element: <PrivateRoute><PostsListStateProvider><WallPage /></PostsListStateProvider></PrivateRoute> },
   registration: { path: '/register', element: <OnlyPublicRoute><RegistrationPage /></OnlyPublicRoute> },
+  timeline: { path: '/timeline', element: <PrivateRoute>Your Timeline</PrivateRoute> },
 }
 
 export function AppRoutes() {
