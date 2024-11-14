3dcf28efd067c81788a82eac5a7ab1dd6de6c857
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2024-11-14 21:21:17 +0100

    add navigation links

--- src/Navigation/Navigation.tsx
+++ src/Navigation/Navigation.tsx
@@ -1,5 +1,6 @@
 import { LogoutButton } from '../Logout/LogoutButton';
 import './Navigation.css';
+import {LinkTo} from "./LinkTo.tsx";
 
 export function Navigation() {
   return (
@@ -8,6 +9,8 @@ export function Navigation() {
         <li>OpenChat</li>
       </ul>
       <ul>
+          <li><LinkTo to={"wall"}>Your Wall</LinkTo></li>
+          <li><LinkTo to={"timeline"}>Your Posts</LinkTo></li>
         <li><LogoutButton /></li>
       </ul>
     </nav>
