beeba7c16d3ab334550b8baa807914c49aa28c0f
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2024-11-27 15:21:11 +0100

    extract NotFoundRoute tests

---
+++ tests/App/NotFoundRoute.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { App } from "../../src/App/App";
+import { User } from "../../src/User/User";
+import { mockUserSession } from '../utils/MockUserSession';
+import { wrapWithRouter } from '../utils/renderHelpers';
+
+describe("Not Found Route", () => {
+  const anUser: User = { id: "1", username: "Pippo", about: "Pippo description" }
+
+  it("renders the not found page when not logged in", () => {
+    mockUserSession({ currentUser: undefined });
+
+    render(<App />, wrapWithRouter({ path: "/not-found" }));
+
+    expect(screen.getByText("Page not found")).toBeInTheDocument();
+  });
+
+  it("renders the not found page when logged in", () => {
+    mockUserSession({ currentUser: anUser });
+
+    render(<App />, wrapWithRouter({ path: "/not-found" }));
+
+    expect(screen.getByText("Page not found")).toBeInTheDocument();
+  });
+});
