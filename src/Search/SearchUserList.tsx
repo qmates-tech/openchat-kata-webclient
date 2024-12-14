0051fb80b2eefb255cd8fbf853215ff4387d0795
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2024-12-14 14:03:00 +0100

    show the users when some users are given

--- src/Search/SearchUserList.tsx
+++ src/Search/SearchUserList.tsx
@@ -8,9 +8,31 @@ export function SearchUserList({ users, retrieving }: SearchUserListProps) {
 
   return (<div className="search-user-list">
     <div data-testid="spinner" aria-busy={retrieving}></div>
-    {!retrieving && hasUsers && <></>}
     {!retrieving && !hasUsers && <NotFound />}
+    {!retrieving && hasUsers && (
+      <table>
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>About</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr key={user.id}>
+              <td>{user.username}</td>
+              <td>{trimmedText(user.about, 25)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    )}
   </div>);
+}
+
+function trimmedText(text: string, maxLength: number) {
+  return text.length > maxLength ? text.slice(0, maxLength) + '...' : text;
 }
 
 function NotFound() {
--- tests/Search/SearchUserList.test.tsx
+++ tests/Search/SearchUserList.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { SearchUserList } from '../../src/Search/SearchUserList';
+import { User } from '../../src/User/User';
 
 describe('SearchUserList', () => {
   it('show "no users found" when no users are given', async () => {
@@ -21,5 +22,18 @@ describe('SearchUserList', () => {
     expect(screen.getByTestId('spinner')).toHaveAttribute('aria-busy', 'false');
     expect(screen.queryByText('No users found.')).toBeInTheDocument();
     expect(screen.queryByText('Generic error')).not.toBeInTheDocument();
+  });
+
+  it('show the users when some users are given', async () => {
+    const users: User[] = [
+      { id: '1', username: 'Alice', about: 'Its Alice' },
+      { id: '2', username: 'Bob', about: 'Bob Description' }
+    ]
+    render(<SearchUserList users={users} retrieving={false} error={undefined} />);
+
+    expect(screen.getByText('Username')).toBeInTheDocument();
+    expect(screen.getByText('About')).toBeInTheDocument();
+    expect(screen.getByText('Alice')).toBeInTheDocument();
+    expect(screen.getByText('Bob')).toBeInTheDocument();
   });
 });
